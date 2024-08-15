3e28b830354b136fec17b9d0ce2fd018e5c245a2
Ahmed Elghazi <126707449+ahmed-elghazi@users.noreply.github.com>
2024-08-14 21:53:53 -0500

    Redact MongoDB User Lik

--- historicalData.py
+++ historicalData.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient  # Import for MongoDB connection
 from pymongo.server_api import ServerApi  # Import for specifying server API version
 
 def hitTheDB(name: str):
-    uri = "mongodb+srv://ahmed:Bqxgz4Mc3eLtUlGj@courses-profs.locvj1l.mongodb.net/?retryWrites=true&w=majority&appName=Courses-Profs"  # MongoDB connection URI
+    uri = ""  # MongoDB connection URI redacted
     client = MongoClient(uri, server_api=ServerApi('1'))  # Connect to MongoDB server
     db = client['university_db']  # Access the database
     collection = db['instructors']  # Access the instructors collection
